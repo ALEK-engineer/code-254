396529c1bfe3f332ab10b37ef02c375abb006fd8
ALEK-engineer <owinoalex394@gmail.com>
2025-03-13 10:55:21 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+
+//OBJECTIVES
+//1. ON Clause
+//2. catch clause with exception object
+//3. catch clause with exception object and stacktrace object
+//4. finally clause
+//5. create our own custom exception
+void main()
+ 
+
